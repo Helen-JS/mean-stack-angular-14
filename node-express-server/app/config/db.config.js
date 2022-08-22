62444f6ee731eddc45d382a2db86de0f6985761e
helenjsdev <helen-balabanova@mail.ru>
2022-08-22 15:51:07 +0200

    fix: change mongodb url

--- node-express-server/app/config/db.config.js
+++ node-express-server/app/config/db.config.js
@@ -1,3 +1,15 @@
 module.exports = {
-  url: "mongodb://localhost:27017/bezkoder_db"
+  url: "mongodb+srv://helen-js:americanmissionary20132014@cluster0.abbib2q.mongodb.net/tutorials"
+  // url: "mongodb://rslang-helen-js.zafit.mongodb.net:27017/admin"
 };
+
+
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://RSLang_App:<password>@rslang-helen-js.zafit.mongodb.net/?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
